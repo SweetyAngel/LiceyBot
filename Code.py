d6d3b661eb3d0b26c0146f3de7de89d87f1d1990
SweetyAngel <119314310+SweetyAngel@users.noreply.github.com>
2022-12-20 13:22:47 +0300

    Fixed properties

--- Code.py
+++ Code.py
@@ -38,6 +38,28 @@ def save_data(users):
         f.write(f'{user.userId}:{user.username} {user.isSubscribed}')
     f.close()
 
+def isInUsers(users, userId, username):
+    for user in users:
+        if user.userId == userId:
+            user.username = username
+            save_data(users)
+            return True
+            break
+    return False
+
+def check(users, userId, username):
+    if not isInUsers(users, userId, username):
+        users = reg(users, userId, username)
+        save_data(users)
+    return users
+
+def findUser(users, userId):
+    for i in range(len(users)-1):
+        if users[i].userId == userId:
+            return i
+            break
+    
+
 token = ''
 bot = telebot.TeleBot(token)
 
@@ -45,19 +67,34 @@ users = users()
 
 @bot.message_handler(commands=['start'])
 def start(msg):
+    users = check(users(), msg.from_user.id, msg.from_user.username)
+    
     if msg.from_user.username != 'None':
         bot.send_message(msg.chat.id, 'Приветствую Вас, {msg.from_user.first_name} {msg.from_user.last_name}(@{msg.from_user.username)! Это официальный бот МБОУ "Лицей" г.о.Протвино. Здесь есть множество функций, о которых Вы можете узнать, открыв меню в левом нижнем углу.')
     else:
         bot.send_message(msg.chat.id, 'Приветствую Вас, {msg.from_user.first_name} {msg.from_user.last_name}! Это официальный бот МБОУ "Лицей" г.о.Протвино. Здесь есть множество функций, о которых Вы можете узнать, открыв меню в левом нижнем углу.')
 
 @bot.message_handler(commands=['help'])
-def hlp(msg):
+def help(msg):
+    users = check(users(), msg.from_user.id, msg.from_user.username)
+
+    save_data(users)
     bot.send_message(msg.chat.id, 'Все вопросы главному разработчику в личные сообщения\n Контакты - Гороховский Альберт(@somebodyoncetoldthewordisgg).')
 
 @bot.message_handler(commands=['subscribe'])
 def successfulSubscription(msg):
+    users = check(users(), msg.from_user.id, msg.from_user.username)
+
+    index = findUser(users, msg.from_user.id)
+    users[i].isSubscribed = True
+    save_data(users)
     bot.send_message(msg.chat.id, 'Вы успешно зарегистрировались на рассылку расписания. Она происходит примерно между 19 и 20 часами каждый рабочий день.\n Если В ызарегестрировались случайно, отправьте команду /unsubscribe.')
 
 @bot.message_handler(commands=['unsubscribe'])
 def successfulUnsubscription(msg):
-    bot.send_message(msg.chat.id, 'Вы отписались от рассылки расписания. Теперь Вам не будет приходить расписание.')    
+    users = check(users(), msg.from_user.id, msg.from_user.username)
+
+    index = findUser(users, msg.from_user.id)
+    users[i].isSubscribed = False
+    save_data(users)
+    bot.send_message(msg.chat.id, 'Вы отписались от рассылки расписания. Теперь Вам не будет приходить расписание.') 
