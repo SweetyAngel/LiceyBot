b62bc24d315a7fe9a7a5fb360d8c8a46f03f9abc
SweetyAngel <119314310+SweetyAngel@users.noreply.github.com>
2022-12-22 10:50:43 +0300

    Big update
    Replaced local users to global users, fixed methods, fixed some bugs

--- Code.py
+++ Code.py
@@ -8,6 +8,8 @@ class User:
 
     def subscribe(self):
         self.isSubscribed = True
+        
+global users
 
 def users():
     f = open('users.py', 'r')
@@ -27,33 +29,31 @@ def users():
                      
     return users
 
-def reg(users, userId, username, isSubscribed = False):
+def reg(userId, username, isSubscribed = False):
     users.append(User(userId, username, isSubscribed))
-    return users
 
-def save_data(users):
+def save_data():
     f = open('users.py', 'w')
     f.write(len(users))
     for user in users:
         f.write(f'{user.userId}:{user.username} {user.isSubscribed}')
     f.close()
 
-def isInUsers(users, userId, username):
+def isInUsers(userId, username):
     for user in users:
         if user.userId == userId:
             user.username = username
-            save_data(users)
+            save_data()
             return True
             break
     return False
 
-def check(users, userId, username):
-    if not isInUsers(users, userId, username):
-        users = reg(users, userId, username)
-        save_data(users)
-    return users
+def check(userId, username):
+    if not isInUsers(userId, username):
+        reg(userId, username)
+        save_data()
 
-def findUser(users, userId):
+def findUser(userId):
     for i in range(len(users)-1):
         if users[i].userId == userId:
             return i
@@ -67,36 +67,36 @@ users = users()
 
 @bot.message_handler(commands=['start'])
 def start(msg):
-    users = check(users(), msg.from_user.id, msg.from_user.username)
+    users = check(msg.from_user.id, msg.from_user.username)
     
     if msg.from_user.username != 'None':
-        bot.send_message(msg.chat.id, 'Приветствую Вас, {msg.from_user.first_name} {msg.from_user.last_name}(@{msg.from_user.username)! Это официальный бот МБОУ "Лицей" г.о.Протвино. Здесь есть множество функций, о которых Вы можете узнать, открыв меню в левом нижнем углу.')
+        bot.send_message(msg.chat.id, 'Приветствую Вас, {msg.from_user.first_name} {msg.from_user.last_name}(@{msg.from_user.username})! Это официальный бот МБОУ "Лицей" г.о.Протвино. Здесь есть множество функций, о которых Вы можете узнать, открыв меню в левом нижнем углу.')
     else:
         bot.send_message(msg.chat.id, 'Приветствую Вас, {msg.from_user.first_name} {msg.from_user.last_name}! Это официальный бот МБОУ "Лицей" г.о.Протвино. Здесь есть множество функций, о которых Вы можете узнать, открыв меню в левом нижнем углу.')
 
 @bot.message_handler(commands=['help'])
 def help(msg):
-    users = check(users(), msg.from_user.id, msg.from_user.username)
+    users = check(msg.from_user.id, msg.from_user.username)
 
-    save_data(users)
+    save_data()
     bot.send_message(msg.chat.id, 'Все вопросы главному разработчику в личные сообщения\n Контакты - Гороховский Альберт(@somebodyoncetoldthewordisgg).')
 
 @bot.message_handler(commands=['subscribe'])
 def successfulSubscription(msg):
-    users = check(users(), msg.from_user.id, msg.from_user.username)
+    users = check(msg.from_user.id, msg.from_user.username)
 
-    index = findUser(users, msg.from_user.id)
+    index = findUser(msg.from_user.id)
     users[i].isSubscribed = True
-    save_data(users)
-    bot.send_message(msg.chat.id, 'Вы успешно зарегистрировались на рассылку расписания. Она происходит примерно между 19 и 20 часами каждый рабочий день.\n Если В ызарегестрировались случайно, отправьте команду /unsubscribe.')
+    save_data()
+    bot.send_message(msg.chat.id, 'Вы успешно зарегистрировались на рассылку расписания. Она происходит примерно между 19 и 20 часами каждый рабочий день.\n Если В ы зарегестрировались случайно, отправьте команду /unsubscribe.')
 
 @bot.message_handler(commands=['unsubscribe'])
 def successfulUnsubscription(msg):
-    users = check(users(), msg.from_user.id, msg.from_user.username)
+    users = check(msg.from_user.id, msg.from_user.username)
 
-    index = findUser(users, msg.from_user.id)
+    index = findUser(msg.from_user.id)
     users[i].isSubscribed = False
-    save_data(users)
+    save_data()
     bot.send_message(msg.chat.id, 'Вы отписались от рассылки расписания. Теперь Вам не будет приходить расписание.') 
 
 bot.infinity_polling()
