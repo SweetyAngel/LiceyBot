e379520c4170433eeb706cd9d95bf10319d4c399
SweetyAngel <119314310+SweetyAngel@users.noreply.github.com>
2022-12-26 12:40:56 +0300

    test

--- start.py
+++ start.py
@@ -1,3 +1,4 @@
+import json
 import telebot
 import datetime
 from telebot import types
@@ -133,11 +134,49 @@ def successfulUnsubscription(msg):
     users[index].isSubscribed = False
     save_data()
     bot.send_message(msg.chat.id, 'Вы отписались от рассылки расписания. Теперь Вам не будет приходить расписание.')
+
+@bot.message_handler(commands=['text'])
+def start(m):
+    count = 10
+    page = 1
+    markup = types.InlineKeyboardMarkup()
+    markup.add(types.InlineKeyboardButton(text='Скрыть', callback_data='unseen'))
+    markup.add(types.InlineKeyboardButton(text=f'{page}/{count}', callback_data=f' '),
+               types.InlineKeyboardButton(text=f'Вперёд --->', callback_data="{\"method\":\"pagination\",\"NumberPage\":" + str(page+1) + ",\"CountPage\":" + str(count) + "}"))
+
+    bot.send_message(m.from_user.id, "Привет!!!", reply_markup = markup)
     
 @bot.message_handler(content_types='text')
 def message_reply(message):
     check(msg.from_user.id, msg.from_user.first_name, msg.from_user.last_name, msg.from_user.username)
     
     bot.send_message(msg.chat.id, 'Бот Вас не понял. Отправьте одну из команд бота')
+    
+@bot.callback_query_handler(func=lambda call:True)
+def callback_query(call):
+    req = call.data.split('_')
+    if req[0] == 'unseen':
+        bot.delete_message(call.message.chat.id, call.message.message_id)
+    elif 'pagination' in req[0]:
+        json_string = json.loads(req[0])
+        count = json_string['CountPage']
+        page = json_string['NumberPage']
+        markup = types.InlineKeyboardMarkup()
+        markup.add(types.InlineKeyboardButton(text='Скрыть', callback_data='unseen'))
+        if page == 1:
+            markup.add(types.InlineKeyboardButton(text=f'{page}/{count}', callback_data=f' '),
+                       types.InlineKeyboardButton(text=f'Вперёд --->',
+                                            callback_data="{\"method\":\"pagination\",\"NumberPage\":" + str(
+                                                page + 1) + ",\"CountPage\":" + str(count) + "}"))
+        elif page == count:
+            markup.add(types.InlineKeyboardButton(text=f'<--- Назад',
+                                            callback_data="{\"method\":\"pagination\",\"NumberPage\":" + str(
+                                                page - 1) + ",\"CountPage\":" + str(count) + "}"),
+                       types.InlineKeyboardButton(text=f'{page}/{count}', callback_data=f' '))
+        else:
+            markup.add(types.InlineKeyboardButton(text=f'<--- Назад', callback_data="{\"method\":\"pagination\",\"NumberPage\":" + str(page-1) + ",\"CountPage\":" + str(count) + "}"),
+                           types.InlineKeyboardButton(text=f'{page}/{count}', callback_data=f' '),
+                           types.InlineKeyboardButton(text=f'Вперёд --->', callback_data="{\"method\":\"pagination\",\"NumberPage\":" + str(page+1) + ",\"CountPage\":" + str(count) + "}"))
+        bot.edit_message_text(f'Страница {page} из {count}', reply_markup = markup, chat_id=call.message.chat.id, message_id=call.message.message_id)
 
 bot.infinity_polling()
