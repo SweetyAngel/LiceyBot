119626bce76960a7d05ebb73f5a3172a8d73db49
Mura <119314310+SweetyAngel@users.noreply.github.com>
2023-01-13 10:56:01 +0300

    Big Update (version 1.0.0)

--- start.py
+++ start.py
@@ -1,4 +1,5 @@
 import json
+import sqlite3
 import threading as thr
 import telebot
 import datetime
@@ -13,17 +14,25 @@ class Date:
         self.year = date[0]
         
 class User:
-    def __init__(self, userId, firstName, secondName, username, isSubscribed = False):
-        self.userId = userId
-        self.firstName = firstName
-        self.secondName = secondName
+    def __init__(self, id, name, surname, username, is_subscribed = False, is_admin = False, last_message = '/start', last_check = datetime.datetime.now()):
+        self.id = id
+        self.name = name
+        self.surname = surname
         self.username = username
-        self.isSubscribed = isSubscribed
+        self.is_subscribed = is_subscribed
+        self.is_admin = is_admin
+        self.last_message = last_message
+        self.last_check = last_check
 
     def subscribe(self):
-        self.isSubscribed = True
+        phr = "UPDATE users SET Is_subscribed=1 WHERE Id=" + str(user.id)
+        cursor.execute(phr)
         
-global users, thrs
+    def unsubscribe(self):
+        phr = "UPDATE users SET Is_subscribed=0 WHERE Id=" + str(user.id)
+        cursor.execute(phr)
+        
+global users, thrs, conn, cursor
 
 def start():
     for thread in thrs:
@@ -43,59 +52,58 @@ def timer(bot):
 
 #Getting list of users
 def users():
-    f = open('users.py', 'r')
-    
-    countOfUsers = int(f.readline())
     users = []
-    
-    for _ in range(countOfUsers):
-        user = f.readline().split(':')
-        userId = user[0]
-        params = user[1].split('\n')[0].split(' ')
-        firstName = params[0]
-        secondName = params[1]
-        username = params[2]
-        isSubscribed = params[3]
-        users.append(User(userId, firstName, secondName, username, isSubscribed))
-                     
-    f.close()
+    all = cursor.fetchall()
+    for i in range (len(all)):
+        id = int(all[i][0])
+        name = all[i][1]
+        surname = all[i][2]
+        username = all[i][3]
+        is_subscribed = str(bool(all[i][4]))
+        is_admin = str(bool(all[i][5]))
+        last_message = all[i][6]
+        last_check = all[i][7]
+        reg(id, name, surname, username, is_subscribed, is_admin, last_message, last_check)
                      
     return users
 
 #Add users in users
-def reg(userId, firstName, secondName, username, isSubscribed = False):
-    users.append(User(userId, firstName, secondName, username, isSubscribed))
+def reg(id, name, surname, username, is_subscribed, is_admin, last_message, last_check):
+    users.append(User(id, name, surname, username, is_subscribed, is_admin, last_message, last_check))
 
 #Save users in users.py
 def save_data():
-    f = open('users.py', 'w')
-    f.write(str(len(users)))
+    cursor.execute("DELETE FROM users;")
+    conn.commit()
     for user in users:
-        f.write(f'\n{user.userId}:{user.firstName} {user.secondName} {user.username} {user.isSubscribed}')
-    f.close()
+        phr = "INSERT INTO users VALUES (" + user.id + ", '" + user.name + "', '" + user.surname + "', " + user.is_subscribed + ", " + user.is_admin + ", '" + user.last_message + "', '" + user.last_check + "');"
+        cursor.execute(phr)
 
+        
 #Check if users exists in users
-def isInUsers(userId, firstName, secondName, username):
+def isInUsers(id, name, surname, username, is_admin, last_message, last_check):
     for user in users:
-        if user.userId == userId:
-            user.firstName = firstName
-            user.secondName = secondName
+        if user.id == id:
+            user.name = name
+            user.surname = surname
             user.username = username
+            user.last_message = last_message
+            user.last_check = last_check
             save_data()
             return True
             break
     return False
 
 #Reg users if he is not in users
-def check(userId, firstName, secondName, username):
-    if not isInUsers(userId, firstName, secondName, username):
-        reg(userId, firstName, secondName, username)
+def check(id, name, surname, username, is_admin, last_message, last_check):
+    if not isInUsers(id, name, surname, username, is_admin, last_message, last_check):
+        reg(id, name, surname, username, is_admin, last_message, last_check)
         save_data()
 
-#Find user in user
-def findUser(userId):
+#Find user in users
+def findUserid):
     for i in range(len(users)):
-        if users[i].userId == userId:
+        if users[i].id == id:
             return i
             break
     
@@ -104,10 +112,12 @@ token = ''
 bot = telebot.TeleBot(token)
 
 users = users()
+conn = sqlite3.connect('bot.sqlite')
+cursor = conn.cursor()
 
 @bot.message_handler(commands=['start'])
 def start(msg):
-    check(msg.from_user.id, msg.from_user.first_name, msg.from_user.last_name, msg.from_user.username)
+    check(msg.from_user.id, msg.from_user.first_name, msg.from_user.last_name, msg.from_user.username, False, False, msg.text, datetime.datetime.now())
     
     if msg.from_user.username != 'None':
         bot.send_message(msg.chat.id, f'Приветствую Вас, {msg.from_user.first_name} {msg.from_user.last_name}(@{msg.from_user.username})! Это официальный бот МБОУ "Лицей" г.о.Протвино. Здесь есть множество функций, о которых Вы можете узнать, открыв меню в левом нижнем углу.')
@@ -116,13 +126,13 @@ def start(msg):
 
 @bot.message_handler(commands=['help'])
 def help(msg):
-    check(msg.from_user.id, msg.from_user.first_name, msg.from_user.last_name, msg.from_user.username)
+    check(msg.from_user.id, msg.from_user.first_name, msg.from_user.last_name, msg.from_user.username, False, False, msg.text, datetime.datetime.now())
 
     bot.send_message(msg.chat.id, 'Все вопросы главному разработчику в личные сообщения\n Контакты - Гороховский Альберт(@somebodyoncetoldmetheworldisgg).')
     
 @bot.message_handler(commands=['website']) 
 def website(msg):
-    check(msg.from_user.id, msg.from_user.first_name, msg.from_user.last_name, msg.from_user.username)
+    check(msg.from_user.id, msg.from_user.first_name, msg.from_user.last_name, msg.from_user.username, False, False, msg.text, datetime.datetime.now())
     
     markup = types.InlineKeyboardMarkup()
     button1 = types.InlineKeyboardButton("Сайт", url='https://protvino-licey.edumsko.ru/')
@@ -131,7 +141,7 @@ def website(msg):
     
 @bot.message_handler(commands=['menu']) 
 def menu(msg):
-    check(msg.from_user.id, msg.from_user.first_name, msg.from_user.last_name, msg.from_user.username)
+    check(msg.from_user.id, msg.from_user.first_name, msg.from_user.last_name, msg.from_user.username, False, False, msg.text, datetime.datetime.now())
     
     markup = types.InlineKeyboardMarkup()
     data = Date(str(datetime.datetime.today()))
@@ -141,8 +151,8 @@ def menu(msg):
     bot.send_message(msg.chat.id, "Нажмите на кнопку, чтобы узнать меню на сегодня", reply_markup=markup)
 
 @bot.message_handler(commands=['subscribe'])
-def successfulSubscription(msg):
-    check(msg.from_user.id, msg.from_user.first_name, msg.from_user.last_name, msg.from_user.username)
+def subscribe(msg):
+    check(msg.from_user.id, msg.from_user.first_name, msg.from_user.last_name, msg.from_user.username, False, False, msg.text, datetime.datetime.now())
 
     index = findUser(msg.from_user.id)
     users[index].subscribe()
@@ -150,8 +160,8 @@ def successfulSubscription(msg):
     bot.send_message(msg.chat.id, 'Вы успешно зарегистрировались на рассылку расписания. Она происходит примерно между 19 и 20 часами каждый рабочий день.\n Если Вы зарегистрировались случайно, отправьте команду /unsubscribe.')
 
 @bot.message_handler(commands=['unsubscribe'])
-def successfulUnsubscription(msg):
-    check(msg.from_user.id, msg.from_user.first_name, msg.from_user.last_name, msg.from_user.username)
+def unsubscribe(msg):
+    check(msg.from_user.id, msg.from_user.first_name, msg.from_user.last_name, msg.from_user.username, False, False, msg.text, datetime.datetime.now())
 
     index = findUser(msg.from_user.id)
     users[index].isSubscribed = False
@@ -171,7 +181,7 @@ def start(m):
     
 @bot.message_handler(content_types='text')
 def message_reply(msg):
-    check(msg.from_user.id, msg.from_user.first_name, msg.from_user.last_name, msg.from_user.username)
+    check(msg.from_user.id, msg.from_user.first_name, msg.from_user.last_name, msg.from_user.username, False, False, msg.text, datetime.datetime.now())
     
     bot.send_message(msg.chat.id, 'Бот Вас не понял. Отправьте одну из команд бота')
     
