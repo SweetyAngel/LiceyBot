9e605c3172b95ad8d7b9f8598a62ee3b6c937e13
Mura <119314310+SweetyAngel@users.noreply.github.com>
2023-01-16 15:51:07 +0300

    Update methods.py

--- methods.py
+++ methods.py
@@ -2,8 +2,3 @@ def antispam():
     for user in users:
         if user.one_min_cnt >= 20:
             user.mute()
-    conn = sqlite3.connect('bot.sqlite', check_same_thread = False)
-    cursor = conn.cursor()
-    cursor.execute(phr)
-    conn.commit()
-    conn.close()
